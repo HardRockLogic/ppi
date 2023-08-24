1c59a0e216871b67d584c1a16b2a23acd76e4e14
HardRockLogic <neiton5001@gmail.com>
2023-08-24 14:28:40 +0300

    prepared new installation script

--- install_to.sh
+++ install_to.sh
@@ -2,12 +2,29 @@
 
 # installation script
 
+cargo build --release
+
 source_path="./target/release/ppi"
 destination_path="/usr/bin/ppi"
 
-if [ -f "$source_path" ]; then
-    sudo cp "$source_path" "$destination_path"
-    echo "File updated successfully to $destination_path"
+move_to() {
+    dest_dir="$1"
+
+    if sudo cp "$source_path" "$dest_dir"; then 
+        echo "moved sucesfully to $dest_dir"
+    else 
+        exit 1
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    echo "no args provided, selected default PATH"
+    move_to $destination_path
 else
-    echo "Source file not fount in $source_path"
+    if [ -d "$1" ]; then
+        move_to $1
+    else 
+        echo "$1 is invalid diractory"
+        exit 1
+    fi 
 fi
